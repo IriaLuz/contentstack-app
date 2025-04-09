640742f51a5f66a86e65859a7508835ceda2cd9e
Iria Suarez <iriasuarez@Mac-2.ultrahub>
2025-04-09 18:59:16 +0100

    secure environment variables

--- src/lib/contentstackClient.ts
+++ src/lib/contentstackClient.ts
@@ -1,14 +1,15 @@
 import { GraphQLClient } from 'graphql-request';
 
-const API_KEY = 'blte07e4904b09bbf13';
-const DELIVERY_TOKEN = 'csb182df8039ca028d4bfd5d70';
-const ENVIRONMENT = 'development';
+const API_KEY = process.env.REACT_APP_CONTENTSTACK_API_KEY || '';
+const DELIVERY_TOKEN = process.env.REACT_APP_CONTENTSTACK_DELIVERY_TOKEN || '';
+const ENVIRONMENT = process.env.REACT_APP_CONTENTSTACK_ENVIRONMENT || '';
 const REGION = 'eu';
 
 const endpoint =
   REGION === 'eu'
     ? `https://eu-graphql.contentstack.com/stacks/${API_KEY}?environment=${ENVIRONMENT}`
     : `https://graphql.contentstack.com/stacks/${API_KEY}?environment=${ENVIRONMENT}`;
+
 
 export const client = new GraphQLClient(endpoint, {
   headers: {
