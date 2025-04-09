4b045188b8fd6b918ab875478e50df131acc98db
Iria Suarez <iriasuarez@Mac-2.ultrahub>
2025-04-09 17:41:03 +0100

    set project

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <div className="bg-blue-500 text-white h-screen flex justify-center items-center">
+      <h1 className="text-4xl font-bold">Hello, Tailwind CSS!</h1>
+    </div>
     </div>
   );
 }
